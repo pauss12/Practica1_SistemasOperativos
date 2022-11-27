1bd78f782139ebd7ab82868790c2388b8d1b0f4e
pauss12 <paulamendezsv@gmail.com>
2022-11-27 14:28:30 +0100

    Funcion cambio Hexadecimal a Decimal hecha, junto con comporbacion que funciona el volcar y limpiar cache

--- MEMsys.c
+++ MEMsys.c
@@ -1,11 +1,20 @@
+//INCLUIMOS TODAS LAS LIBRERIAS QUE NECESITEMOS
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
 
+//CREAMOS LOS DEFINE ----------------------------------------------------------------------------------------------------------------
 #define NUM_FILAS 8
-#define MAX_LINE 100
+#define MAX_LINEA 100
 #define TAM_LINEA 8
 #define LRAM 1024
 
+//ESTRUCTURA ----------------------------------------------------------------------------------------------------------------
 typedef struct {
 	
 	unsigned short int ETQ;
@@ -13,40 +22,49 @@ typedef struct {
 	
 }T_LINEA_CACHE;
 
+//PROTOTIPOS DE LAS FUNCIONES ----------------------------------------------------------------------------------------------------------------
 void LimpiaCACHE(T_LINEA_CACHE tbl[NUM_FILAS]);
 void VuelcaCACHE(T_LINEA_CACHE *tbl);
 void Parseaaddr(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque);
 void TrataFallo(T_LINEA_CACHE *tbl, char *MRAM, int ETQ, int linea, int bloque);
+int cambioHexadecimalADecimal(long long numero );
 
-//VARIABLES GLOBALES
+//VARIABLES GLOBALES ----------------------------------------------------------------------------------------------------------------
 	int globaltime = 0;
 	int numfallos = 0;
 	unsigned char Simul_RAM[4096];
 
-
+//MAIN ----------------------------------------------------------------------------------------------------------------
 int main(int argc, char* argv[]){
 	
 	//Crear las variables
 	T_LINEA_CACHE linea_cache[NUM_FILAS];
 	
 	int variable1 = 0xFF;
-	int variable2 = 0x23F;
+	int variable2 = 0x23;
 	
 	//Llamar a las funciones
-	LimpiaCACHE(linea_cache);
 	VuelcaCACHE(linea_cache);
 	
 	return 0;
 }
 
-int cambioHexadecimalADecimal(int variable1, int variable2){
+//FUNCION CAMBIAR DE HEXADECIMAL A DECIMAL
+int cambioHexadecimalADecimal(long long numero ){
 	
-	if(isdigit(variable1, variable2)){
-		return variable1, variable2 - '0';
-		
+	//Variables
+	int i = 0;
+	int variable_auxiliar;
+	int numero_devuelto = 0;
+	
+	while(numero != 0){
+		variable_auxiliar = numero % 10;
+		numero = numero / 10;
+		numero_devuelto = numero_devuelto + (variable_auxiliar * (pow(2, i)));
+		++i;
 	}
 	
-	return 0;
+	return numero_devuelto;
 }
 
 void LimpiaCACHE(T_LINEA_CACHE tbl[NUM_FILAS]){ 
@@ -54,7 +72,7 @@ void LimpiaCACHE(T_LINEA_CACHE tbl[NUM_FILAS]){
     for(int i=0; i<NUM_FILAS; i++){
     	tbl[i].ETQ= 0xFF;
     	for(int j=0; j<TAM_LINEA;j++){
-    		tbl[i].Datos[j]= 0x23F;
+    		tbl[i].Datos[j]= 0x23;
 		}
 	}
     
@@ -67,5 +85,6 @@ void VuelcaCACHE(T_LINEA_CACHE *tbl){
 		for(int j=0; j<TAM_LINEA; j++){
 			printf("%02X ", tbl[i].Datos[j]);
 		}
+		printf("\n");
 	}
 }
